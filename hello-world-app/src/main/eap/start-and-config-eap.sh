c68c6b3bb07e1575b0fec95d11ed25e840d3121f
Marco Klaassen <mklaasse@redhat.com>
2024-10-02 13:53:14 +0200

    configure syslog service

--- hello-world-app/src/main/eap/start-and-config-eap.sh
+++ hello-world-app/src/main/eap/start-and-config-eap.sh
@@ -6,7 +6,7 @@ while ! nc -z localhost 9990; do
 done
 
 # configure syslog-handler
-/opt/jboss-eap-74/bin/jboss-cli.sh '/subsystem=logging/syslog-handler=SYSLOG:add(server-address="slc12467.agcs-tem-dev.ec1.aws.aztec.cloud.allianz",port=2514,facility=local-use-6,syslog-format=RFC3164,level=INFO,app-name=JBOSS_SAMPLE_APPLICATION,enabled=true,hostname=jboss-sample-application)' --connect
+/opt/jboss-eap-74/bin/jboss-cli.sh '/subsystem=logging/syslog-handler=SYSLOG:add(server-address="rsyslog-server.jboss-eap-test.svc.cluster.local",port=2514,facility=local-use-6,syslog-format=RFC3164,level=INFO,app-name=JBOSS_SAMPLE_APPLICATION,enabled=true,hostname=jboss-sample-application)' --connect
 /opt/jboss-eap-74/bin/jboss-cli.sh '/subsystem=logging/root-logger=ROOT:add-handler(name=SYSLOG)' --connect
 
 # monitor logs
