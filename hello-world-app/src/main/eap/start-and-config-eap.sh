2079afb673b8d3a5a694fd8154b96c1d421b3072
Marco Klaassen <mklaasse@redhat.com>
2024-10-04 11:09:08 +0200

    change jboss cli cmd

--- hello-world-app/src/main/eap/start-and-config-eap.sh
+++ hello-world-app/src/main/eap/start-and-config-eap.sh
@@ -6,8 +6,8 @@ while ! nc -z localhost 9990; do
 done
 
 # configure syslog-handler
-/opt/jboss-eap-74/bin/jboss-cli.sh '/subsystem=logging/syslog-handler=SYSLOG:add(server-address="rsyslog-server.jboss-eap-test.svc.cluster.local",port=2514,facility=local-use-6,syslog-format=RFC3164,level=INFO,app-name=JBOSS_SAMPLE_APPLICATION,enabled=true,hostname=jboss-sample-application)' --connect
-/opt/jboss-eap-74/bin/jboss-cli.sh '/subsystem=logging/root-logger=ROOT:add-handler(name=SYSLOG)' --connect
+/opt/eap/bin/jboss-cli.sh '/subsystem=logging/syslog-handler=SYSLOG:add(server-address="rsyslog-server.jboss-eap-test.svc.cluster.local",port=2514,facility=local-use-6,syslog-format=RFC3164,level=INFO,app-name=JBOSS_SAMPLE_APPLICATION,enabled=true,hostname=jboss-sample-application)' --connect
+/opt/eap/bin/jboss-cli.sh '/subsystem=logging/root-logger=ROOT:add-handler(name=SYSLOG)' --connect
 
 # monitor logs
 tail -f /opt/jboss-eap-74/standalone/log/server.log
